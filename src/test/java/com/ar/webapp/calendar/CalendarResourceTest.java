b7b0dc4d877eeb6039498d6191f113c227671ed0
rettnera <rettnera@MSGN01121.int.root.msg.ag>
2015-06-11 09:46:56 +0200

    Created another test method for a GET request.

--- src/test/java/com/ar/webapp/calendar/CalendarResourceTest.java
+++ src/test/java/com/ar/webapp/calendar/CalendarResourceTest.java
@@ -3,18 +3,23 @@
  */
 package com.ar.webapp.calendar;
 
+import static com.jayway.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
+import javax.ws.rs.core.Response.Status;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
 
 /**
  * @author Alexander Rettner
@@ -23,16 +28,30 @@ import org.junit.Test;
 public class CalendarResourceTest {
 
 	/**
+	 * Test method for
+	 * {@link com.ar.webapp.books.BookService#getBook(com.ar.webapp.books.BookRequest)}
+	 * .
+	 */
+	@Test
+	public final void testGetBookByTitle() {
+		Response response = given().contentType(ContentType.TEXT).expect()
+				.contentType(ContentType.TEXT)
+				.statusCode(Status.OK.getStatusCode()).when()
+				.get("http://localhost:8080/calendar-webapp/webapi/data/text");
+
+		String responseText = response.asString();
+
+		assertEquals(new CalendarResource().getDataAsString(), responseText);
+	}
+
+	/**
 	 * Test method for {@link com.ar.webapp.calendar.CalendarResource#getData()}
 	 * .
 	 * 
 	 * @throws JAXBException
-	 * 
-	 * @throws IOException
-	 * @throws JsonMappingException
-	 * @throws JsonParseException
 	 */
 	@Test
+	@Ignore
 	public final void testGetData() throws JAXBException {
 		final JAXBContext context = JAXBContext.newInstance(MyJaxbBean.class);
 
